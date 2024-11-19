0847bb54679ba8cc7a1e47d99f7824de9903a020
BunsDND <itsmevance19@gmail.com>
2024-11-19 15:38:15 +0800

    NOV19-LAB

--- app/(tabs)/location.jsx
+++ app/(tabs)/location.jsx
@@ -1,6 +1,6 @@
 import { View, Text, ScrollView, TextInput, StyleSheet, FlatList, Image } from 'react-native';
 import React, { useState } from 'react';
-import { ImageBackground } from 'react-native-web';
+import { ImageBackground } from 'react-native';
 
 
 
@@ -44,6 +44,11 @@ const Location = () => {const [search, setSearch] = useState(''); // State to ho
   return (
     <ImageBackground source={require('../../assets/bg.png')} style={styles.background}>
       <ScrollView>
+      <View style={styles.headerContainer}> 
+        <Image source={require('../../assets/logo.png')} style={styles.logobabaw }/>
+
+      <Text style={styles.tabTitle}>Location</Text>
+        </View>
         <View style={styles.container}>
           <TextInput
             style={styles.searchBar}
@@ -98,12 +103,37 @@ const Location = () => {const [search, setSearch] = useState(''); // State to ho
 };
 
 const styles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop:10,
+    marginLeft:10,
+    marginriGHT:10,
+  },
+  logobabaw: {
+    width: 40, 
+    height: 40,
+    borderRadius: 10,
+    position: 'absolute',
+    left: 0, // Positions the logo on the right side of the container
+  },
+  tabTitle: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#1C3269',
+    textShadowColor: 'rgba(0, 0, 0, 0.55)',
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 5,
+    flex: 1, // Ensures the text takes up available space
+    textAlign: 'center',
+  },
   background: {
     flex: 1,
     resizeMode: 'cover',
   },
   container: {
-    padding: 10,
+    padding: 16,
   },
   searchBar: {
     height: 40,
--- constants/icons.js
+++ constants/icons.js
@@ -2,6 +2,9 @@ import home from "../assets/icons/home.png";
 import location from "../assets/icons/location.png";
 import ticket from "../assets/icons/ticket.png";
 import profile from "../assets/icons/profile.png";
+import bg from "../assets/bg.png";
+import logo from "../assets/logo.png";
+
 
 
 export default {
@@ -9,4 +12,6 @@ export default {
   location, 
   ticket, 
   profile,
+  bg,
+  logo,
  };
